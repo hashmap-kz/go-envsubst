a9465b127e56f167da7e3e78f063dbbe0774dba6
alexey.zh <mailbox.stdio@gmail.com>
2024-07-27 15:18:16 +0500

    clean code

--- main.go
+++ main.go
@@ -3,21 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/hashmap.kz/go-envsubst/pkg/tok"
-	"github.com/hashmap.kz/go-envsubst/pkg/util"
 	"io"
 	"log"
 	"os"
 )
-
-func tokenizeFile(fname string) *tok.Tokenlist {
-	b, err := util.ReadFile(fname)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	tl, _ := tok.Tokenize(b)
-	return tl
-}
 
 func main() {
 	bytes, err := io.ReadAll(os.Stdin)
