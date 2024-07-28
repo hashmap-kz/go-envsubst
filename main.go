5dd85199b1dcd1aee0adaabbbc6f7682545134cf
alexey.zh <mailbox.stdio@gmail.com>
2024-07-28 07:53:08 +0500

    add usage

--- main.go
+++ main.go
@@ -13,7 +13,18 @@ var (
 	dryRun = flag.Bool("dry-run", false, "")
 )
 
+var usage = `Usage: genvsubst [options...] <input>
+Options:
+  -dry-run   Print replacement table
+`
+
 func main() {
+	flag.Usage = func() {
+		_, err := fmt.Fprint(os.Stderr, fmt.Sprintf(usage))
+		if err != nil {
+			log.Println("internal error on fprintf:", err)
+		}
+	}
 	flag.Parse()
 
 	bytes, err := io.ReadAll(os.Stdin)
